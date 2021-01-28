d9cebc7aeb8dc20d99ff75c6c11cdb6ac6b818db
alperenozil <necdet.ozil@hacettepe.edu.tr>
2021-01-28 19:36:38 +0300

    small change

--- app/src/main/java/tech/ozil/kotlinsecond/MainActivity.kt
+++ app/src/main/java/tech/ozil/kotlinsecond/MainActivity.kt
@@ -2,10 +2,23 @@ package tech.ozil.kotlinsecond
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    internal lateinit var tapMeButton: Button
+    internal lateinit var alperenText: TextView
+    internal lateinit var TAG: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        TAG="alperenozil"
+        alperenText=findViewById(R.id.textViewMiddle)
+        tapMeButton=findViewById(R.id.buttonOverTextViewMiddle)
+        tapMeButton.setOnClickListener(View.OnClickListener {
+            Log.d(TAG, "onCreate: tap")
+        })
     }
 }
